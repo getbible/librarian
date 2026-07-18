004384307cb700362ff00376fe65b6a4e8b881ac
eWɘyn <5607939+Llewellynvdm@users.noreply.github.com>
2026-07-18 22:42:53 +0200

    Export typed GetBible errors

--- src/getbible/__init__.py
+++ src/getbible/__init__.py
@@ -1,4 +1,24 @@
+from .errors import (
+    DataValidationError,
+    GetBibleError,
+    InvalidReferenceError,
+    ScriptureNotFoundError,
+    TranslationNotFoundError,
+    UpstreamUnavailableError,
+)
 from .getbible_book_number import GetBibleBookNumber
-from .getbible_reference import GetBibleReference
-from .getbible_reference import BookReference
+from .getbible_reference import BookReference, GetBibleReference
 from .getbible import GetBible
+
+__all__ = [
+    "BookReference",
+    "DataValidationError",
+    "GetBible",
+    "GetBibleBookNumber",
+    "GetBibleError",
+    "GetBibleReference",
+    "InvalidReferenceError",
+    "ScriptureNotFoundError",
+    "TranslationNotFoundError",
+    "UpstreamUnavailableError",
+]
