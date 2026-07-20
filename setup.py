0b55f3ec97b6f1c0c901df3132d0e8f3e3b6fad9
eWɘyn <5607939+Llewellynvdm@users.noreply.github.com>
2026-07-20 18:17:49 +0200

    Restore production hardening source archive

--- setup.py
+++
@@ -1,6 +0,0 @@
-"""Compatibility shim for tooling that still invokes setup.py directly."""
-
-from setuptools import setup
-
-
-setup()
--- src/getbible/trie_node.py
+++
@@ -1,5 +0,0 @@
-class TrieNode:
-    def __init__(self):
-        self.children = {}
-        self.book_number = None
-
